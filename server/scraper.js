714b96b734f2f302886418ab896d22f34283c596
Craig Treulieb <ctreulieb@gmail.com>
2014-10-02 01:00:04 -0400

    have scraper removing pages, unnecessary stuff like books and shirts, leaving only actual issues and publishers

--- server/scraper.js
+++ server/scraper.js
@@ -6,14 +6,17 @@ request('http://www.previewsworld.com/support/previews_docs/orderforms/archive/2
 	if(!error && response.statusCode == 200) {
 		var lines = body.split("\r\n");
 		lines.splice(0,lines.indexOf("PREMIER PUBLISHERS")+1);
-		console.log(lines[0]);
+		lines.splice(lines.indexOf("COLLECTED EDITIONS"), 1);
+		lines.splice(lines.indexOf("BOOKS"),lines.length - (lines.indexOf("BOOKS") + 1));
 		for( var i = 0; i < lines.length; i++)
 		{
-			
-			if(reg.exec(lines[i])){
-				console.log(lines[i]);
+			if((lines[i].indexOf("#") === -1 && lines[i].indexOf("\t") > -1) || reg.test(lines[i]) || lines[i].indexOf("1 FOR $1") > -1 ){
 				lines.splice(i,1);
+				--i;
 			}
 		}
+		var file = fs.createWriteStream('array.txt');
+		lines.forEach(function(v) { file.write(v + '\n'); });
+		file.end();
 	}
 })
