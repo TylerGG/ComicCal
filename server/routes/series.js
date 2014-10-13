c0d09db24eb2f543f6f0914d513e1360cbe396e7
Daniel Mills <mills.dma@gmail.com>
2014-10-13 18:55:12 -0400

    add subscribe/unsubscribe routes

--- server/routes/series.js
+++ server/routes/series.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Series = require('../models/Series');
 var Issue = require('../models/Issue');
+var Subscription = require('../models/Subscription');
 
 router.param('seriesId',function(req,res,next,id) {
 	Series.findOne({_id:id},function(err,series) {
@@ -24,6 +25,47 @@ router.get('/', function(req, res) {
 router.get('/:seriesId',function(req,res) {
 	res.json(req.series);
 });
+
+router.post('/:seriesId/subscribe',function(req,res) {
+
+	var form = req.body;
+	if(form.api_key === undefined || form.user_id === undefined) {
+		res.status(400).json({'Result':'Credentials missing'});
+	}
+
+	Subscription.findOne({series_id:form.series_id,user_id:form.user_id},function(err,subscription) {
+		if(err) throw err;
+		if(subscription) {
+			res.status(400).json({'Result':'Subscription already exists'});	
+		} else {
+			Subscription.create({series_id:form.series_id,user_id:form.user_id},function(err,subscription){
+				if(err) throw err;
+				res.json({'Result':'OK',subscription:subscription});
+			});
+		}
+	});
+});
+
+router.post('/:seriesId/unsubscribe',function(req,res) {
+
+	var form = req.body;
+	if(form.api_key === undefined || form.user_id === undefined) {
+		res.status(400).json({'Result':'Credentials missing'});
+	}
+
+	Subscription.findOne({series_id:form.series_id,user_id:form.user_id},function(err,subscription) {
+		if(err) throw err;
+		if(!subscription) {
+			res.status(400).json({'Result':'Subscription not found.'});	
+		} else {
+			Subscription.remove({series_id:form.series_id,user_id:form.user_id},function(err) {
+				if(err) throw err;
+				res.status(200).json({'Result':'OK'});
+			});
+		}
+	});
+});
+
 
 //Get all issues for series
 router.get('/:seriesId/issues',function(req,res) {
