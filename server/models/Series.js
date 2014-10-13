1ff9ec86fd0f6334f9f8bb72f97af245693b2ac2
Daniel Mills <mills.dma@gmail.com>
2014-10-13 18:53:31 -0400

    Added subscription model, added timestamps and pre save update function

--- server/models/Series.js
+++ server/models/Series.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose')
 
 var ObjectId = mongoose.Schema.ObjectId;
+var updateTimestamps = require('./util').updateTimestamps;
 
 var SeriesSchema = new mongoose.Schema({
 	name: { type:String,require:true, index:{unique:true}},
-	publisher_id: { type:ObjectId,require:true}
+	publisher_id: { type:ObjectId,require:true},
+	created_at: { type:Date,require:true},
+	updated_at: { type:Date,require:true}
 });
+
+SeriesSchema.pre('save',updateTimestamps);
 
 module.exports = mongoose.model('Series',SeriesSchema);
---
+++ server/models/Subscription.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.ObjectId;
+
+var SubscriptionSchema = new mongoose.Schema({
+	series_id: { type:ObjectId, require:true },
+	user_id: { type:ObjectId,require:true },
+	created_at:{type:Date,require:true},
+	updated_at:{type:Date,require:true}
+});
+
+SubscriptionSchema.pre('save',function(next) {
+	var d = new Date();
+	if(!this.created_at) this.created_at = d;
+	this.updated_at = d;
+	next();
+});
+
+
+module.exports = mongoose.model('Subscription',SubscriptionSchema);
