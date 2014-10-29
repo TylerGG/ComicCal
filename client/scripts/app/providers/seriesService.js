eaa1aee91948814592e9c17b4f6efcbe22a62e79
Tyler Garrow <tylerg.ca@gmail.com>
2014-10-29 08:59:40 -0700

    Added proper calls to SeriesService

--- client/scripts/app/providers/seriesService.js
+++ client/scripts/app/providers/seriesService.js
@@ -3,26 +3,27 @@ var app = angular.module('comicCal');
 
 /* needs refactor to call server */
 
-app.factory('SeriesService',[ '$http', 'ApiKeyService', function($http, ApiKeyService) {
+app.factory('SeriesService',[ '$rootScope', '$http', 'ApiKeyService', function($rootScope, $http, ApiKeyService) {
 	var factory = {};
+	var rootUrl = $rootScope.root_url;
 
 	factory.subscribedSeries = function(user_id) {
 		var credentials = ApiKeyService.getCredentials();
-		return $http.get('/users/subscriptions/' + credentials.user_id,{
+		return $http.get(rootUrl + '/users/subscriptions/' + credentials.user_id,{
  			 headers: { 'comical-user-id': credentials.user_id , 'comical-api-token':credentials.api_key }
 	    });
 	};
 
 	factory.publishers = function() {
 		var credentials = ApiKeyService.getCredentials();
-		return $http.get('/publishers',{
+		return $http.get(rootUrl + '/publishers',{
  			 headers: { 'comical-user-id': credentials.user_id , 'comical-api-token':credentials.api_key }
 	    });
 	};
 
 	factory.seriesByPublisher = function (publisher) {
 		var credentials = ApiKeyService.getCredentials();
-		return $http.get('/publishers/' + publisher + '/series',{
+		return $http.get(rootUrl + '/publishers/' + publisher + '/series',{
  			 headers: { 'comical-user-id': credentials.user_id , 'comical-api-token':credentials.api_key }
 	    });
 	};
